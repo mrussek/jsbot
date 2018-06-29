6b4f5fa9dcad5333c3ffed4dc663372cdcebe65f
Max Russek <maxwell.russek@workday.com>
2018-06-29 12:11:25 -0600

    Got fulfillment talking to dialogflow.

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const rp = require('request-promise')
 
 const getNotifications = (req, res) => {
-    const pretty = JSON.stringify(req.body, null, 4)
-    console.log(pretty)
-    res.send(pretty).end()
+    const dialog = req.body
+    console.log(JSON.stringify(dialog))
+
+    const intent = dialog.queryResult.intent.displayName
+    console.log(`intent: ${intent}`)
+
+    switch (intent) {
+        case "get-notifications":
+        res.json({
+            fulfillmentText: "Your notifications are not available"
+        }).status(200).end()
+        case "get-user-repos":
+        res.json({
+            fulfillmentText: "Getting user repos"
+        }).status(200).end()
+
+        default:
+        res.json({
+            fulfillmentText: "Lol what happened"
+        }).status(200).end()
+    }
 }
 
 module.exports = {
