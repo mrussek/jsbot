0acb16d401319becf6ef2face6c96e69417fab6f
Max Russek <maxwell.russek@workday.com>
2018-06-30 00:55:24 -0600

    Uptake dialogflow fulfillment library (🎉).

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const rp = require('request-promise')
 
+const { WebhookClient } = require('dialogflow-fulfillment');
+const { Card, Suggestion } = require('dialogflow-fulfillment');
+
 const getNotifications = (req, res) => {
     const get = (uri, qs) => {
         console.log(`Hitting: ${uri}`)
+        console.log(`Params: ${JSON.stringify(qs)}`)
         return rp.get({
             uri: uri,
             qs: qs,
@@ -13,45 +17,53 @@ const getNotifications = (req, res) => {
         })
     }
 
-    const dialog = req.body
-    console.log(JSON.stringify(dialog))
+    const agent = new WebhookClient({ request: req, response: res });
+    console.log('Dialogflow Request headers: ' + JSON.stringify(req.headers));
+    console.log('Dialogflow Request body: ' + JSON.stringify(req.body));
 
-    const queryResult = dialog.queryResult
-    const intent = queryResult.intent.displayName
-    const parameters = queryResult.parameters
-    const firstName = parameters["given-name"]
-    const lastName = parameters["last-name"]
-    console.log(`intent: ${intent}`)
-
-
-    switch (intent) {
-        case "get-notifications":
-            res.json({
-                fulfillmentText: "Your notifications are not available"
-            }).status(200).end()
-            break;
-        case "get-user-repos":
-            console.log("Getting user repos")
-            get("https://api.github.com/search/users", {
-                q: `${firstName} ${lastName}`
-            }).then(jake => {
-                console.log(JSON.stringify(jake))
-                return get(jake.items[0].repos_url)
-            }).then(repos => {
-                console.log(JSON.stringify(repos))
-                const names = repos.map(repo => repo.name)
-
-                res.json({
-                    fulfillmentText: `${firstName} ${lastName}'s repositories are: ${names.join(", ")}`
-                }).status(200).end()
-            })
-        break
-        default:
-            res.json({
-                fulfillmentText: "Lol what happened"
-            }).status(200).end()
-            break;
+    function welcome(agent) {
+        agent.add(`Welcome to my agent!`)
     }
+
+    function fallback(agent) {
+        agent.add(`I didn't understand`)
+        agent.add(`I'm sorry, can you try again?`)
+    }
+
+    // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
+    // below to get this function to be run when a Dialogflow intent is matched
+    function getNots(agent) {
+      agent.add(`Sorry! I can't handle that yet`)
+
+      console.log("Fulfillment \"get-notifications\" completed")
+    }
+
+    function getUserRepos(agent) {
+        const userName = `${agent.parameters["given-name"]} ${agent.parameters["last-name"]}`
+
+        console.log(`Looking up user: ${userName}`)
+
+        return get("https://api.github.com/search/users", {
+            q: userName
+        }).then(users => {
+            console.log(JSON.stringify(users))
+            return get(users.items[0].repos_url)
+        }).then(repos => {
+            const repoNames = repos.map(repo => repo.name)
+
+            agent.add(`I found repos by ${userName}: ${repoNames.join(", ")}`)
+
+            console.log("Fulfillment \"get-user-repos\" completed")
+        })
+    }
+
+    let intentMap = new Map();
+    intentMap.set('Default Welcome Intent', welcome)
+    intentMap.set('Default Fallback Intent', fallback)
+    intentMap.set('get-notifications', getNots)
+    intentMap.set('get-user-repos', getUserRepos)
+
+    agent.handleRequest(intentMap);
 }
 
 module.exports = {
